7784625d1fea1694b8e46a695fc2a550b36ea29f
Niluferose <nilufergulciftci@hotmail.com>
2025-04-27 18:34:42 +0300

    Odev1 : Variables

---
+++ app/src/main/java/com/example/homework_1_2/variables/CreatingVariable.kt
@@ -0,0 +1,98 @@
+package com.example.homework_1_2.variables
+
+fun main(){
+    val sehir = "Tekirdag"
+    println("Şehir : $sehir")
+    val ulke = "Turkiye"
+    println("Ülke : $ulke")
+    val telefon = 539448394853
+    println("Telefon : $telefon")
+    val posta_kodu = 59100
+    println("Posta Kodu : $posta_kodu")
+    val email = "nilufergulciftci@hotmail.com"
+    println("Email : $email")
+    val meslek = "Bilgisayar Mühendisi"
+    println("Meslek : $meslek")
+    val stok_miktari = 2000
+    println("Stok Miktarı : $stok_miktari tane")
+    val musteri_adi = "Nilufer"
+    println("Müşteri Adı : $musteri_adi")
+    val bakiye = 0.0
+    println("Bakiye : $bakiye TL")
+    val dogum_gunu = "4 Agustos"
+    println("Doğum Günü : $dogum_gunu")
+    val maas = 0
+    println("Maaş : $maas")
+    val medeni_durum = "Bekar"
+    println("Medeni Durum : $medeni_durum")
+    val urun_yorum = "Güzel ürün"
+    println("Ürün Yorum : $urun_yorum")
+    val odeme_tarihi = "4 Mayıs"
+    println("Ödeme Tarihi : $odeme_tarihi")
+    val odeme = "kredi kartı"
+    println("Ödeme : $odeme")
+    val siparis_adeti = 40
+    println("Sipariş Adeti : $siparis_adeti")
+    val araba_modeli = "Tesla"
+    println("Araba Modeli : $araba_modeli")
+    val kitap_adi = "Dr. Moreau'nun Adası"
+    println("Kitap Adı : $kitap_adi")
+    val yayinlanma_tarihi = 1896
+    println("Yayınlama Tarihi : $yayinlanma_tarihi")
+    val indirim_miktari = 20
+    println("İndirim Miktarı : %$indirim_miktari")
+    val oda_sayisi = 4
+    println("Oda Sayısı : $oda_sayisi oda")
+    val enlem = 4.368945
+    println("Enlem : $enlem")
+    val boylam = 18.3293
+    println("Boylam : $boylam")
+    val urun_adi = "Kitap ayracı"
+    println("Ürün Adı : $urun_adi")
+    val yemek_fiyati = 450
+    println("Yemek Fiyatı : $yemek_fiyati TL")
+    val marka = "Apple"
+    println("Marka : $marka")
+    val muzik_adi = "No Suprises"
+    println("Müzik Adı : $muzik_adi")
+    val video_suresi = 4
+    println("Video Süresi : $video_suresi dakika")
+    val urun_puani = 3.6
+    println("Ürün Puanı : $urun_puani")
+    val resim_adi = "Starry_night.png"
+    println("Resim Adı : $resim_adi")
+    val dosya_formati = "deneme.pdf"
+    println("Dosya Formatı : $dosya_formati")
+    val renk = "mor"
+    println("Renk : $renk")
+    val renk_kodu = "#453"
+    println("Renk Kodu : $renk_kodu")
+    val telefon_modeli = "Iphone 16 Pro"
+    println("Telefon Modeli : $telefon_modeli")
+    val ekran_boyutu = 13.5
+    println("Ekran Boyutu : $ekran_boyutu")
+    val agirlik = 5.4
+    println("Ağırlık : $agirlik")
+    val ulusal_gun = "19 Mayıs Gençlik Bayramı"
+    println("Ulusal Gün : $ulusal_gun")
+    val tatil_gunu = "Pazar günü"
+    println("Tatil Günü : $tatil_gunu")
+    val rezervasyon_gunu = "3 Agustos"
+    println("Rezervasyon Günü : $rezervasyon_gunu")
+    val sokak_adi = "Sahin sokak"
+    println("Sokak Adı : $sokak_adi")
+    val otobus_hatti = "19S"
+    println("Otobüs Hattı : $otobus_hatti")
+    val kalan_dakika = 12
+    println("Kalan Dakika : $kalan_dakika dakina")
+    val takip_kodu = 274
+    println("Takip Kodu : $takip_kodu")
+    val kupon_suresi = 20
+    println("Kupon Süresi : $kupon_suresi")
+    val kupon_kodu = 4234
+    println("Kupon Kodu : $kupon_kodu")
+    val fatura_adresi = "Hurriyet Mahallesi"
+    println("Fatura Adresi : $fatura_adresi")
+
+
+}
